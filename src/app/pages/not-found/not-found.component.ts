a6c5fb45f763c57aee4ae157287cfa35356fdd84
Vasyl Pashko <vasyl.pashko@gmail.com>
2025-01-01 22:03:49 +0200

    shutdown

--- src/app/pages/not-found/not-found.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-not-found',
-  standalone: true,
-  imports: [],
-  templateUrl: './not-found.component.html',
-})
-export class NotFoundComponent {}
